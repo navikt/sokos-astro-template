7dd02c15906b559a6fd5e1582dbb819f3c1bdb23
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-05-27 15:25:45 +0200

    Første oppsett basert på tms-microfrontend-template-ssr
    Co-authored-by: sasoria <sergio.alberto.arevalo.soria@nav.no>
    Co-authored-by: nima-hakimi <nima.hakimi@nav.no>

---
+++ astro.config.mjs
@@ -0,0 +1,45 @@
+import { defineConfig } from "astro/config";
+import { rollupImportMapPlugin } from "rollup-plugin-import-map";
+import importmap from "./importmap.json";
+import react from "@astrojs/react";
+import node from "@astrojs/node";
+import prefixer from "postcss-prefix-selector";
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+    assetsPrefix: "https://cdn.nav.no/min-side/tms-microfrontend-test",
+  },
+  vite: {
+    css: {
+      postcss: {
+        plugins: [
+          prefixer({
+            prefix: ".sokos-up-astro-template", // brukes for å unngå å lekke css ut av mikrofrontenden
+            ignoreFiles: [/module.css/],
+          }),
+        ],
+      },
+    },
+  },
+  integrations: [
+    react(),
+    {
+      name: "importmap-externals",
+      hooks: {
+        "astro:build:setup": ({ vite, target }) => {
+          if (target === "client") {
+            vite.build.rollupOptions["external"] = [
+              "react",
+              "react-dom"
+            ];
+          }
+        },
+      },
+    },
+  ],
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+});
