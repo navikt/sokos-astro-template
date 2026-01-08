4ce4b30d5b45d07a41acc505538ef7cf8fa4b424
Mudasar Cheema <mudasar_cheema@hotmail.com>
2026-01-08 13:53:40 +0100

    legger til biome linter (#76)

--- src/types/Employee.ts
+++ src/types/Employee.ts
@@ -1,5 +1,8 @@
-import { EmployeeListeSchema, EmployeeSchema } from "@schema/EmployeeSchema";
-import { z } from "zod";
+import type {
+	EmployeeListeSchema,
+	EmployeeSchema,
+} from "@schema/EmployeeSchema";
+import type { z } from "zod";
 
 export type EmployeeList = z.infer<typeof EmployeeListeSchema>;
 
