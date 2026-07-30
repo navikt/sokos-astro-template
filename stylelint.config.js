e0dd9a725f3ecd3ef9650e9893f8feaca99b684c
mudasar187 <mudasar_cheema@hotmail.com>
2026-07-31 00:40:48 +0200

    migrere til aksel v8

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,9 @@
 export default {
 	extends: ["stylelint-config-standard", "@navikt/aksel-stylelint/recommended"],
 	rules: {
-		"selector-class-pattern": null,
+		"selector-class-pattern":
+			"^[a-z][a-z0-9-]*(__[a-z][a-z0-9-]*)*(--[a-z][a-z0-9-]*)?$",
+		"custom-property-pattern": "[a-z][a-zA-Z0-9-]+",
 		"selector-pseudo-class-no-unknown": [
 			true,
 			{
